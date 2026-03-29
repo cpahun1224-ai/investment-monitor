62754e8268a8a05b8caeafcb6bf07d4e8b9c9024
jihuny <jihuny@users.noreply.github.com>
2026-03-29 21:14:09 +0900

    use DATABASE_PATH env var for SQLite persistence

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./investment.db"
+import os
+_db_path = os.getenv("DATABASE_PATH", "./investment.db")
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{_db_path}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
